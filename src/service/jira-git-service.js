11493bf912f10ffe2a2498cd78c259cb036518b0
Samuel Richard Magny <s.magny@robotiq.com>
2019-07-14 18:46:44 -0400

    Added current git branch update

--- src/service/jira-git-service.js
+++ src/service/jira-git-service.js
@@ -1,4 +1,5 @@
 const { getCurrentBranchName } = require("./git-service");
+const { setIssueFixVersionAndCreateFixVersion } = require("./jira-issues-service");
 
 const JIRA_ISSUE_MATCHER = /((?!([A-Z0-9a-z]{1,10})-?$)[A-Z]{1}[A-Z0-9]+-\d+)/g;
 
@@ -8,4 +9,16 @@ async function extractIssuesFromCurrentBranch() {
   return branchName.match(JIRA_ISSUE_MATCHER);
 }
 
-module.exports = { extractIssuesFromCurrentBranch };
+async function setCurrentBranchIssuesFixVersion(auth, fixVersion) {
+  const issueKeys = await extractIssuesFromCurrentBranch();
+
+  return await Promise.all(issueKeys.map(async (issueKey) => {
+    try {
+      return await setIssueFixVersionAndCreateFixVersion(auth, issueKey, fixVersion);
+    } catch(error) {
+      return { error };
+    }
+  }));
+}
+
+module.exports = { extractIssuesFromCurrentBranch, setCurrentBranchIssuesFixVersion };
